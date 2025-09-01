a36ea980b2afc2cc47954edd2b268c14f92710cd
Tianling Shen <cnsztl@immortalwrt.org>
2025-09-01 22:50:33 +0800

    kernel: add scsi ufs support
    This will be used by some rk3576 boards.
    
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- target/linux/rockchip/modules.mk
+++ target/linux/rockchip/modules.mk
@@ -69,3 +69,18 @@ define KernelPackage/saradc-rockchip/description
 endef
 
 $(eval $(call KernelPackage,saradc-rockchip))
+
+define KernelPackage/scsi-ufs-rockchip
+  SUBMENU:=$(BLOCK_MENU)
+  TITLE:=Rockchip UFS host controller support
+  DEPENDS:=+kmod-scsi-ufs-platform
+  KCONFIG:=CONFIG_SCSI_UFS_ROCKCHIP
+  FILES:=$(LINUX_DIR)/drivers/ufs/host/ufs-rockchip.ko
+  AUTOLOAD:=$(call AutoLoad,45,ufs-rockchip,1)
+endef
+
+define KernelPackage/scsi-ufs-rockchip/description
+  Support for the Rockchip specific additions to UFSHCD platform
+endef
+
+$(eval $(call KernelPackage,scsi-ufs-rockchip))
